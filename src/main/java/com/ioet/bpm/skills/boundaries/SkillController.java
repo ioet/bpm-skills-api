8bf900487ab301a5e1d2a0069579b0c4dc20677f
Lukas Ziefle <lziefle@ioet.com>
2018-09-25 17:23:20 -0500

    Added missing crud methods

--- src/main/java/com/ioet/bpm/skills/boundaries/SkillController.java
+++ src/main/java/com/ioet/bpm/skills/boundaries/SkillController.java
@@ -1,10 +1,13 @@
 package com.ioet.bpm.skills.boundaries;
 
 import com.ioet.bpm.skills.domain.Skill;
+import com.ioet.bpm.skills.exception.SkillNotFoundException;
 import com.ioet.bpm.skills.repositories.SkillRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/skills")
@@ -22,9 +25,36 @@ public class SkillController {
         return new ResponseEntity<>(skills, HttpStatus.OK);
     }
 
+    @GetMapping("/{id}")
+    public Skill getSkill(@PathVariable(value = "id") String skillId) {
+        return skillRepository.findById(skillId)
+                .orElseThrow(() -> new SkillNotFoundException("Skill", "id", skillId));
+    }
+
     @PostMapping
     public ResponseEntity<Skill> create(@RequestBody Skill skill) {
         Skill skillCreated = skillRepository.save(skill);
         return new ResponseEntity<>(skillCreated, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable(value = "id") String skillId) {
+        Skill skill = skillRepository.findById(skillId)
+                .orElseThrow(() -> new SkillNotFoundException("Skill", "id", skillId));
+
+        skillRepository.delete(skill);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{id}")
+    public Skill updateSkill(@PathVariable(value = "id") String skillId, @Valid @RequestBody Skill skillDetails) {
+        Skill skill = skillRepository.findById(skillId)
+                .orElseThrow(() -> new SkillNotFoundException("Skill", "id", skillId));
+
+        skill.setName(skillDetails.getName());
+        skill.setDescription(skillDetails.getDescription());
+
+        return skillRepository.save(skill);
     }
 }
