e7fca76e9f1bf77da0263d4821b1ff09c612511e
Lukas Ziefle <lziefle@ioet.com>
2018-09-26 09:26:28 -0500

    returning only 404

--- src/main/java/com/ioet/bpm/skills/boundaries/SkillController.java
+++ src/main/java/com/ioet/bpm/skills/boundaries/SkillController.java
@@ -1,13 +1,13 @@
 package com.ioet.bpm.skills.boundaries;
 
 import com.ioet.bpm.skills.domain.Skill;
-import com.ioet.bpm.skills.exception.SkillNotFoundException;
 import com.ioet.bpm.skills.repositories.SkillRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/skills")
@@ -20,41 +20,47 @@ public class SkillController {
     }
 
     @GetMapping
-    public ResponseEntity<Iterable> findSkills() {
+    public ResponseEntity<Iterable> getAllSkills() {
         Iterable<Skill> skills = this.skillRepository.findAll();
         return new ResponseEntity<>(skills, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public Skill getSkill(@PathVariable(value = "id") String skillId) {
-        return skillRepository.findById(skillId)
-                .orElseThrow(() -> new SkillNotFoundException("Skill", "id", skillId));
+    public ResponseEntity<Skill> getSkill(@PathVariable(value = "id") String skillId) {
+        Optional<Skill> skillOptional = skillRepository.findById(skillId);
+        return skillOptional.map(
+                skill -> new ResponseEntity<>(skill, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping
-    public ResponseEntity<Skill> create(@RequestBody Skill skill) {
+    public ResponseEntity<Skill> createSkill(@RequestBody Skill skill) {
         Skill skillCreated = skillRepository.save(skill);
         return new ResponseEntity<>(skillCreated, HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> delete(@PathVariable(value = "id") String skillId) {
-        Skill skill = skillRepository.findById(skillId)
-                .orElseThrow(() -> new SkillNotFoundException("Skill", "id", skillId));
+    public ResponseEntity<?> deleteSkill(@PathVariable(value = "id") String skillId) {
+        Optional<Skill> skill = skillRepository.findById(skillId);
+        if (!skill.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
 
-        skillRepository.delete(skill);
-
-        return ResponseEntity.ok().build();
+        skillRepository.delete(skill.get());
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
-    public Skill updateSkill(@PathVariable(value = "id") String skillId, @Valid @RequestBody Skill skillDetails) {
-        Skill skill = skillRepository.findById(skillId)
-                .orElseThrow(() -> new SkillNotFoundException("Skill", "id", skillId));
+    public ResponseEntity<Skill> updateSkill(@PathVariable(value = "id") String skillId, @Valid @RequestBody Skill skillDetails) {
+        Optional<Skill> skillOptional = skillRepository.findById(skillId);
+        if (!skillOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
 
+        Skill skill = skillOptional.get();
         skill.setName(skillDetails.getName());
         skill.setDescription(skillDetails.getDescription());
-
-        return skillRepository.save(skill);
+        skillRepository.save(skill);
+        return new ResponseEntity<>(skill, HttpStatus.OK);
     }
 }
