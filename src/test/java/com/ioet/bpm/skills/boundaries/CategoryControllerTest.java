0fe32ecfec94a823a2cfd7535e3c9cd55b8e182a
SpraylnlPray <danielwildegger@gmx.de>
2018-11-29 14:46:56 -0500

    Implement tests for CategoryController

--- src/test/java/com/ioet/bpm/skills/boundaries/CategoryControllerTest.java
+++ src/test/java/com/ioet/bpm/skills/boundaries/CategoryControllerTest.java
@@ -1,29 +1,24 @@
 package com.ioet.bpm.skills.boundaries;
 
 import com.ioet.bpm.skills.domain.Category;
-import com.ioet.bpm.skills.domain.Skill;
 import com.ioet.bpm.skills.repositories.CategoryRepository;
-import com.ioet.bpm.skills.repositories.SkillRepository;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import java.util.Optional;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
-
-import java.util.List;
-import java.util.Optional;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CategoryControllerTest {
-    @Mock
-    private SkillRepository skillRepository;
-
     @Mock
     private CategoryRepository categoryRepository;
 
@@ -31,42 +26,39 @@ public class CategoryControllerTest {
     private CategoryController categoryController;
 
     @Test
-    public void categoriesAreListedUsingTheRepository() throws Exception {
+    public void categoriesAreListedUsingTheRepository() {
 
         ResponseEntity<Iterable> categories = categoryController.getAllCategories();
 
         Assert.assertEquals(HttpStatus.OK, categories.getStatusCode());
-        Mockito.verify(categoryRepository, Mockito.times(1)).findAll();
+        verify(categoryRepository).findAll();
     }
 
     @Test
-    public void whenACategoryIsCreatedTheNewCategoryIsReturned() throws Exception {
-        Category categoryToCreate = Mockito.mock(Category.class);
-        Category categoryCreated = Mockito.mock(Category.class);
-        Optional<Category> category = Optional.of(Mockito.mock(Category.class));
-        Mockito.when(categoryRepository.findById(Mockito.anyString())).thenReturn(category);
-        Mockito.when(categoryToCreate.getId()).thenReturn("1");
-        Mockito.when(categoryRepository.save(categoryToCreate)).thenReturn(categoryCreated);
+    public void whenACategoryIsCreatedTheNewCategoryIsReturned() {
+        Category categoryToCreate = mock(Category.class);
+        Category categoryCreated = mock(Category.class);
+        when(categoryRepository.save(categoryToCreate)).thenReturn(categoryCreated);
 
         ResponseEntity<?> skillCreatedResponse = categoryController.createCategory(categoryToCreate);
 
         Assert.assertEquals(categoryCreated, skillCreatedResponse.getBody());
         Assert.assertEquals(HttpStatus.CREATED, skillCreatedResponse.getStatusCode());
-        Mockito.verify(categoryRepository, Mockito.times(1)).save(categoryToCreate);
+        verify(categoryRepository).save(categoryToCreate);
     }
 
     @Test
     public void findCategoryById() {
-        Category categoryToFind = Mockito.mock(Category.class);
+        Category categoryToFind = mock(Category.class);
         categoryController.getCategory(categoryToFind.getId());
-        Mockito.verify(categoryRepository, Mockito.times(1)).findById(categoryToFind.getId());
+        verify(categoryRepository).findById(categoryToFind.getId());
     }
 
     @Test
     public void theBodyContainsTheCategoryFromTheRepository() {
         String categoryIdToFind = "id";
-        Category categoryFound = Mockito.mock(Category.class);
-        Mockito.when(categoryRepository.findById(categoryIdToFind)).thenReturn(Optional.of(categoryFound));
+        Category categoryFound = mock(Category.class);
+        when(categoryRepository.findById(categoryIdToFind)).thenReturn(Optional.of(categoryFound));
 
         ResponseEntity<Category> existingCategoryResponse = categoryController.getCategory(categoryIdToFind);
 
@@ -76,13 +68,58 @@ public class CategoryControllerTest {
 
     @Test
     public void deleteCategory() {
-        Category categoryToDelete = Mockito.mock(Category.class);
-        Mockito.when(categoryRepository.findById(categoryToDelete.getId())).thenReturn(Optional.of(categoryToDelete));
+        Category categoryToDelete = mock(Category.class);
+        when(categoryRepository.findById(categoryToDelete.getId())).thenReturn(Optional.of(categoryToDelete));
 
         ResponseEntity<?> deletedCategoryResponse = categoryController.deleteCategory(categoryToDelete.getId());
 
         assertNull(deletedCategoryResponse.getBody());
         assertEquals(HttpStatus.NO_CONTENT, deletedCategoryResponse.getStatusCode());
-        Mockito.verify(categoryRepository, Mockito.times(1)).delete(categoryToDelete);
+        verify(categoryRepository).delete(categoryToDelete);
+    }
+
+    @Test
+    public void updateCategory() {
+        Category categoryUpdated = mock(Category.class);
+        Optional<Category> categoryFound = Optional.of(mock(Category.class));
+
+        String idCategoryToUpdate = "id";
+        Category categoryToUpdate = mock(Category.class);
+
+        when(categoryRepository.findById(idCategoryToUpdate)).thenReturn(categoryFound);
+        when(categoryRepository.save(categoryToUpdate)).thenReturn(categoryUpdated);
+
+        ResponseEntity<Category> updatedCategoryResponse =
+                categoryController.updateCategory(idCategoryToUpdate, categoryToUpdate);
+
+        assertEquals(categoryUpdated, updatedCategoryResponse.getBody());
+        assertEquals(HttpStatus.OK, updatedCategoryResponse.getStatusCode());
+        verify(categoryRepository).save(categoryToUpdate);
+    }
+
+    @Test
+    public void whenUpdatingNotExistingCategoryThenReturn404() {
+        String idCategoryToUpdate = "id";
+        when(categoryRepository.findById(idCategoryToUpdate)).thenReturn(Optional.empty());
+
+        ResponseEntity<Category> updatedCategoryResponse =
+                categoryController.updateCategory(idCategoryToUpdate, mock(Category.class));
+
+        assertEquals(HttpStatus.NOT_FOUND, updatedCategoryResponse.getStatusCode());
+        verify(categoryRepository).findById(idCategoryToUpdate);
+        verify(categoryRepository, never()).save(any());
+    }
+
+    @Test
+    public void whenDeletingNotExistingCategoryThenReturn404() {
+        String idCategoryToDelete = "id";
+        when(categoryRepository.findById(idCategoryToDelete)).thenReturn(Optional.empty());
+
+        ResponseEntity deletedCategoryResponse =
+                categoryController.deleteCategory(idCategoryToDelete);
+
+        assertEquals(HttpStatus.NOT_FOUND, deletedCategoryResponse.getStatusCode());
+        verify(categoryRepository, never()).delete(any());
+        verify(categoryRepository).findById(idCategoryToDelete);
     }
 }
